314dd6c00d3c9a28e5a36a63fe52e10b376b0754
yusluv1234 <yusluv124@gamil.com>
2020-12-12 23:28:54 +0100

    still on navbar components

--- src/components/button/Button.jsx
+++ src/components/button/Button.jsx
@@ -10,9 +10,9 @@ const COLOR = ['primary', 'blue', 'red', 'green'];
 export const Button = ({
     children, type, onClick, buttonStyle, buttonSize, buttonColor
 }) => {
-    const checkButtonStyle = STYLES.includes(buttonStyle) ? buttonStyle : STYLES[[0]]
-    const checkButtonSize = SIZES.includes(buttonSize) ? buttonSize : SIZES[[0]]
-    const checkButtonColor = COLOR.includes(buttonColor) ? buttonColor : COLOR[[0]]
+    const checkButtonStyle = STYLES.includes(buttonStyle) ? buttonStyle : STYLES[0]
+    const checkButtonSize = SIZES.includes(buttonSize) ? buttonSize : SIZES[0]
+    const checkButtonColor = COLOR.includes(buttonColor) ? buttonColor : null;
 
     return (
         <button className={`btn ${checkButtonStyle} ${checkButtonSize} ${checkButtonColor}`} onClick={onClick}
--- src/components/navbar/Navbar.js
+++ src/components/navbar/Navbar.js
@@ -1,7 +1,8 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { Link } from 'react-router-dom';
 import { MdFingerprint } from 'react-icons/md'
 import { FaBars, FaTimes } from 'react-icons/fa';
+import { IconContext } from 'react-icons/lib';
 import { Button } from '../button/Button';
 
 import './Navbar.css';
@@ -21,10 +22,15 @@ const Navbar = () => {
         }
     };
 
+    useEffect(() => {
+        showButton();
+    }, [])
+
     window.addEventListener('resize', showButton);
 
     return (
         <>
+        <IconContext.Provider value={{ color: '#fff' }}>
         <div className='navbar'>
             <div className='navbar-container container'>
                 <Link to='#' className='navbar-logo'>
@@ -64,6 +70,7 @@ const Navbar = () => {
             </ul>
             </div>
         </div>
+        </IconContext.Provider>
         </>
     )
 }
